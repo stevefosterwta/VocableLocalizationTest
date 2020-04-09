af1349b3be13509974ddadf956e6dac765f3cc5e
Steve Foster <steve.foster@willowtreeapps.com>
2020-04-09 12:18:42 -0400

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "VocableLocalizationTest",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(name: "VocableLocalizationTest", targets: ["VocableLocalizationTest"]),
+    ],
+    targets: [
+        .target(name: "VocableLocalizationTest", dependencies: [])
+    ]
+)
